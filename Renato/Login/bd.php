350cd95fb0bca69f9cf48c7ef69b8dc72e9cd10a
RenatoDroz <renato-drozdek@hotmail.com>
2017-11-24 23:36:52 -0200

    Add files via upload

---
+++ Renato/Login/bd.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$dbname = "estudo_dirigido_bd";
+$dbusername = "root";
+$dbpassword = "";
+
+try
+{
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname",$dbusername,$dbpassword,
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+
+catch(PDOException $e)
+{
+    $error = $e->getMessage();
+    echo $error;
+}
+?>
